7a07c65954c0fbdf8d191c815d5e5ccf3c148ba1
OM Miles <miles@ommiles.com>
2021-06-26 12:16:17 -0700

    write create method in seeds file

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -16,3 +16,19 @@ resp = RestClient::Request.execute(
 
 brewery_data = JSON.parse(resp.body)
 breweries = brewery_data
+
+breweries.each do |brewery|
+    Brewery.create(
+        name: brewery["name"],
+        brewery_type: brewery["brewery_type"],
+        street: brewery["street"],
+        city: brewery["city"],
+        state: brewery["state"],
+        postal_code: brewery["postal_code"],
+        country: brewery["country"],
+        phone: brewery["phone"],
+        website_url: brewery["website_url"],
+        latitude: brewery["latitude"],
+        longitude: brewery["longitude"]
+    )
+end
