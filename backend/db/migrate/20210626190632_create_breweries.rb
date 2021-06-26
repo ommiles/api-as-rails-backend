aecaf5887df3afb2a36fbcaeb5304bf5d795e5f2
OM Miles <miles@ommiles.com>
2021-06-26 12:07:07 -0700

    generate migration

---
+++ backend/db/migrate/20210626190632_create_breweries.rb
@@ -0,0 +1,19 @@
+class CreateBreweries < ActiveRecord::Migration[6.1]
+  def change
+    create_table :breweries do |t|
+      t.string :name
+      t.string :brewery_type
+      t.string :street
+      t.string :city
+      t.string :state
+      t.string :postal_code
+      t.string :country
+      t.string :phone
+      t.string :website_url
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
